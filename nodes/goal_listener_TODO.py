d55d4ff6def9f009e1e0982889393bf6a8a25494
Kokou <vivian1998kokou@gmail.com>
2023-04-20 01:45:57 +0200

    rviz

--- nodes/goal_listener_TODO.py
+++ nodes/goal_listener_TODO.py
@@ -5,34 +5,19 @@ import actionlib
 from geometry_msgs.msg import PoseStamped
 from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
 
-import yaml
-
-
 client = actionlib.SimpleActionClient('move_base', MoveBaseAction)
 
-waypoints = []
-
-def listener():
-    rospy.init_node('goal_listener')
-    rospy.Subscriber('ko/move_base_simple/goal', PoseStamped, callback)
-
-
-def callback_(goal):
-    # rospy.loginfo(goal)
-    print(move_base_(goal))
 
 def callback(goal):
 
-    waypoints.append(goal)
+    ## Write your code here 
 
-    if len(waypoints) == 3:
-        continue_ = True
-        while continue_ and len(waypoints) > 0:
-            continue_ = move_base_(waypoints.pop(0))
+    ### .....
+    pass
 
 
 
-def move_base_(goal_):
+def move_base_to(goal_):
     # client = actionlib.SimpleActionClient('move_base', MoveBaseAction)
 
     client.wait_for_server()
@@ -54,28 +39,9 @@ def move_base_(goal_):
     # return client.get_result()
 
 
-def save_goal(goal):
-    pass
-
-    rospy.set_param('charging_stations/station_1/position/x',
-                    goal.pose.position.x)
-
-    input_data = {
-        'station_1': {
-            'position': {
-                'x': goal.pose.position.x,
-                'y': goal.pose.position.y,
-                'z': goal.pose.position.z,
-            }
-        }
-    }
-
-    path = '/home/lissassi/catkin_ws/src/tp_ros/config/charging_station'
-
-    with open(f'{path}.yaml', 'w') as f:
-        yaml.dump(input_data, f, sort_keys=False)
-
-    print('Written to file successfully')
+def listener():
+    rospy.init_node('goal_listener')
+    rospy.Subscriber('ko/move_base_simple/goal', PoseStamped, callback)
 
 
 if __name__ == '__main__':

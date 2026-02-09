6ccf048188a2a3a56c86b4614a34453eed6edd0f
LISSASSI Kokou Chéridhan <49092618+cheridhan@users.noreply.github.com>
2026-02-09 23:20:40 +0100

    Refactor install_setup.sh for Turtlebot3 setup
    Removed Turtlebot3 package installations and updated git clone command for Turtlebot3 repository.

--- install_setup.sh
+++ install_setup.sh
@@ -8,31 +8,18 @@ sudo apt-get update
 echo "Installing nano editor..."
 sudo apt-get install nano -y nano
 
-# Installing Turtlebot3 ROS package
-echo "Installing Turtlebot3 ROS package..."
-sudo apt-get install ros-noetic-turtlebot3 -y
-
-# Installing Turtlebot3 simulation package
-echo "Installing Turtlebot3 simulation package..."
-sudo apt-get install ros-noetic-turtlebot3-simulations -y
-
 # Setting up TURTLEBOT3_MODEL environment variable
 sudo echo "export TURTLEBOT3_MODEL=burger">>.bashrc
-source .bashrc
 
 # Installing map server
 echo "Installing Map Server"
 sudo apt-get install ros-noetic-map-server -y
 
-cd ~/catkin_ws/src
-echo "Installing tp_ros package"
-git clone -b workout_communication https://github.com/cheridhan/tp_ros
-
-echo "Install Turtlebot3 teleop package"
-sudo apt-get install ros-noetic-turtlebot3-teleop -y
+git clone -b noetic https://github.com/ROBOTIS-GIT/turtlebot3.git catkin_ws/src/turtlebot3
 
 cd ~/catkin_ws
 catkin_make
 
 
+source .bashrc
 echo "Installation and Setup complete successfully"
